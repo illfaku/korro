517786399ad219cbc65a9c1e6b772de542887a08
Vladimir Konstantinov <illfaku@gmail.com>
2015-04-26 12:14:53 +0600

    added extractors for HttpMethod

--- src/main/scala/io/cafebabe/http/api/HttpMethod.scala
+++ src/main/scala/io/cafebabe/http/api/HttpMethod.scala
@@ -4,9 +4,15 @@ package io.cafebabe.http.api
  * @author Vladimir Konstantinov
  * @version 1.0 (4/17/2015)
  */
-object HttpMethod extends Enumeration {
-  type HttpMethod = Value
-  val CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE = Value
+class HttpMethod(name: String) {
+  def unapply(req: RestRequest): Option[RestRequest] = if (req.method.equalsIgnoreCase(name)) Some(req) else None
 }
 
-
+object GET extends HttpMethod("GET")
+object POST extends HttpMethod("POST")
+object PUT extends HttpMethod("PUT")
+object DELETE extends HttpMethod("DELETE")
+object HEAD extends HttpMethod("HEAD")
+object CONNECT extends HttpMethod("CONNECT")
+object OPTIONS extends HttpMethod("OPTIONS")
+object TRACE extends HttpMethod("TRACE")
