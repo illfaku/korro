e46b32592f8cb3a526ce99154c1e38b9f5c4930c
Vladimir Konstantinov <illfaku@gmail.com>
2015-06-25 13:57:57 +0600

    refactored HttpMethod

--- src/main/scala/io/cafebabe/http/server/api/HttpMethod.scala
+++ src/main/scala/io/cafebabe/http/server/api/HttpMethod.scala
@@ -1,11 +1,30 @@
+/*
+ * Copyright (C) 2015  Vladimir Konstantinov, Yuriy Gintsyak
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.cafebabe.http.server.api
 
 /**
+ * TODO: Add description.
+ *
  * @author Vladimir Konstantinov
  * @version 1.0 (4/17/2015)
  */
-class HttpMethod(name: String) {
-  def unapply(req: HttpRequest): Option[HttpRequest] = if (req.method.equalsIgnoreCase(name)) Some(req) else None
+sealed abstract class HttpMethod(name: String) {
+  def unapply(req: HttpRequest): Option[(String, HttpRequest)] =
+    if (req.method.equalsIgnoreCase(name)) Some(req.path, req) else None
 }
 
 object GET extends HttpMethod("GET")
