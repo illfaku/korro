b10b1a79941b79c9733fbad3061385ee3a9087f0
Vladimir Konstantinov <illfaku@gmail.com>
2015-06-25 13:43:16 +0600

    moved HttpContent factory to impl

--- src/main/scala/io/cafebabe/http/server/api/HttpContent.scala
+++ src/main/scala/io/cafebabe/http/server/api/HttpContent.scala
@@ -16,61 +16,7 @@
  */
 package io.cafebabe.http.server.api
 
-import io.cafebabe.http.server.api.exception.BadRequestException
-import io.netty.handler.codec.http.FullHttpRequest
-import io.netty.handler.codec.http.HttpHeaders.Names._
 import org.json4s.JValue
-import org.json4s.ParserUtil.ParseException
-import org.json4s.native.JsonMethods.parse
-
-import java.nio.charset.Charset
-
-/**
- * TODO: Add description.
- *
- * @author Vladimir Konstantinov
- * @version 1.0 (6/23/2015)
- */
-object HttpContent {
-
-  private val DefaultCharset = Charset.forName("UTF-8")
-
-  private val ContentType = """([^;]+)(?:; charset=([\w-]+))""".r
-
-  private[server] def apply(request: FullHttpRequest): HttpContent = {
-    if (contentLength(request) > 0) {
-      contentType(request) match {
-        case ("text/plain", charset) => TextHttpContent(request.content.toString(charset))
-        case ("application/json", charset) =>
-          try JsonHttpContent(parse(request.content.toString(charset))) catch {
-            case e: ParseException => throw new BadRequestException(s"Fail to parse json content: ${e.getMessage}")
-          }
-        case (mime, _) => throw new BadRequestException(s"Unsupported Content-Type: $mime.")
-      }
-    } else EmptyHttpContent
-  }
-
-  private def contentLength(request: FullHttpRequest): Int = {
-    val header = request.headers.get(CONTENT_LENGTH)
-    if (header != null) {
-      try header.toInt catch {
-        case e: NumberFormatException => 0
-      }
-    } else 0
-  }
-
-  private def contentType(request: FullHttpRequest): (String, Charset) = {
-    request.headers.get(CONTENT_TYPE) match {
-      case ContentType(mime, charset) =>
-        if (charset != null) {
-          try mime -> Charset.forName(charset) catch {
-            case e: IllegalArgumentException => throw new BadRequestException(s"Unsupported charset: $charset.")
-          }
-        } else mime -> DefaultCharset
-      case _ => "text/plain" -> DefaultCharset
-    }
-  }
-}
 
 /**
  * TODO: Add description.
--- src/main/scala/io/cafebabe/http/server/impl/NettyHttpRequest.scala
+++ src/main/scala/io/cafebabe/http/server/impl/NettyHttpRequest.scala
@@ -16,8 +16,14 @@
  */
 package io.cafebabe.http.server.impl
 
-import io.cafebabe.http.server.api.{HttpContent, HttpHeaders, HttpRequest, QueryParams}
+import io.cafebabe.http.server.api._
+import io.cafebabe.http.server.api.exception.BadRequestException
+import io.netty.handler.codec.http.HttpHeaders.Names._
 import io.netty.handler.codec.http.{FullHttpRequest, QueryStringDecoder}
+import org.json4s.ParserUtil.ParseException
+import org.json4s.native.JsonMethods._
+
+import java.nio.charset.Charset
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable
@@ -36,7 +42,7 @@ object NettyHttpRequest {
       path.substring(pathPrefix.length),
       NettyQueryParams(request),
       NettyHttpHeaders(request),
-      HttpContent(request)
+      NettyHttpContent(request)
     )
   }
 }
@@ -71,5 +77,52 @@ object NettyHttpHeaders {
       result += key -> (header.getValue :: list)
     }
     new HttpHeaders(Map.empty ++ result)
+  }
+}
+
+/**
+ * TODO: Add description.
+ *
+ * @author Vladimir Konstantinov
+ * @version 1.0 (6/23/2015)
+ */
+object NettyHttpContent {
+
+  private val DefaultCharset = Charset.forName("UTF-8")
+
+  private val ContentType = """([^;]+)(?:; charset=([\w-]+))""".r
+
+  def apply(request: FullHttpRequest): HttpContent = {
+    if (contentLength(request) > 0) {
+      contentType(request) match {
+        case ("text/plain", charset) => TextHttpContent(request.content.toString(charset))
+        case ("application/json", charset) =>
+          try JsonHttpContent(parse(request.content.toString(charset))) catch {
+            case e: ParseException => throw new BadRequestException(s"Fail to parse json content: ${e.getMessage}")
+          }
+        case (mime, _) => throw new BadRequestException(s"Unsupported Content-Type: $mime.")
+      }
+    } else EmptyHttpContent
+  }
+
+  private def contentLength(request: FullHttpRequest): Int = {
+    val header = request.headers.get(CONTENT_LENGTH)
+    if (header != null) {
+      try header.toInt catch {
+        case e: NumberFormatException => 0
+      }
+    } else 0
+  }
+
+  private def contentType(request: FullHttpRequest): (String, Charset) = {
+    request.headers.get(CONTENT_TYPE) match {
+      case ContentType(mime, charset) =>
+        if (charset != null) {
+          try mime -> Charset.forName(charset) catch {
+            case e: IllegalArgumentException => throw new BadRequestException(s"Unsupported charset: $charset.")
+          }
+        } else mime -> DefaultCharset
+      case _ => "text/plain" -> DefaultCharset
+    }
   }
 }
