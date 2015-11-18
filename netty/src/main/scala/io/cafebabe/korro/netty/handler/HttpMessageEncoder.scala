4a463a21595363992649854165d5839dddaa705b
Vladimir Konstantinov <illfaku@gmail.com>
2015-11-18 23:06:26 +0600

    completed HttpMessageEncoder

--- netty/src/main/scala/io/cafebabe/korro/netty/handler/HttpMessageEncoder.scala
+++ netty/src/main/scala/io/cafebabe/korro/netty/handler/HttpMessageEncoder.scala
@@ -1,17 +1,13 @@
 package io.cafebabe.korro.netty.handler
 
 import io.cafebabe.korro.api.http.HttpParams.HttpParams
-import io.cafebabe.korro.api.http.HttpRequest
-import io.cafebabe.korro.api.http.HttpResponse
-import io.cafebabe.korro.api.http._
-import io.cafebabe.korro.netty.ByteBufUtils
-import io.cafebabe.korro.netty.convert.{HttpHeadersConverter, QueryParamsConverter}
+import io.cafebabe.korro.api.http.{HttpContent, HttpMessage, HttpRequest, HttpResponse, _}
+import io.cafebabe.korro.netty.ByteBufUtils.toByteBuf
 
-import io.netty.buffer.Unpooled
-import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.{ChannelHandlerContext, DefaultFileRegion}
 import io.netty.handler.codec.MessageToMessageEncoder
-import io.netty.handler.codec.http.HttpMethod
-import io.netty.handler.codec.http.{HttpMessage => NettyHttpMessage, _}
+import io.netty.handler.codec.http.{HttpMessage => NettyMessage, HttpMethod, _}
 
 import java.util
 
@@ -20,6 +16,7 @@ import java.util
   *
   * @author Vladimir Konstantinov
   */
+@Sharable
 class HttpMessageEncoder extends MessageToMessageEncoder[HttpMessage] {
 
   override def encode(ctx: ChannelHandlerContext, msg: HttpMessage, out: util.List[AnyRef]): Unit = {
@@ -28,7 +25,7 @@ class HttpMessageEncoder extends MessageToMessageEncoder[HttpMessage] {
 
       case req: HttpRequest =>
         val request = new DefaultHttpRequest(
-          HttpVersion.HTTP_1_1, HttpMethod.valueOf(req.method), QueryParamsConverter.toNetty(req.path, req.parameters)
+          HttpVersion.HTTP_1_1, HttpMethod.valueOf(req.method), prepareUri(req.path, req.parameters)
         )
         setHeaders(request, req)
         out add request
@@ -39,17 +36,30 @@ class HttpMessageEncoder extends MessageToMessageEncoder[HttpMessage] {
         out add response
     }
 
-    msg.content match {
-
-      case content: MemoryHttpContent =>
-        val buf = if (content.length > 0) ByteBufUtils.toByteBuf(content.bytes) else Unpooled.EMPTY_BUFFER
-        val lastChunk = new DefaultLastHttpContent(buf)
-
-      case content: FileHttpContent =>
+    if (msg.content.length > 0) {
+      encodeContent(msg.content, out)
+    } else {
+      out add LastHttpContent.EMPTY_LAST_CONTENT
     }
   }
 
-  private def setHeaders(nettyMsg: NettyHttpMessage, msg: HttpMessage): Unit = {
+  private def encodeContent(content: HttpContent, out: util.List[AnyRef]): Unit = content match {
+
+    case c: MemoryHttpContent =>
+      out add new DefaultLastHttpContent(c.bytes)
+
+    case c: FileHttpContent =>
+      out add new DefaultFileRegion(c.file.toFile, c.pos, c.length - c.pos)
+      out add LastHttpContent.EMPTY_LAST_CONTENT
+  }
+
+  private def prepareUri(path: String, parameters: HttpParams): String = {
+    val encoder = new QueryStringEncoder(path)
+    parameters foreach { case (name, values) => values.foreach(encoder.addParam(name, _)) }
+    encoder.toString
+  }
+
+  private def setHeaders(nettyMsg: NettyMessage, msg: HttpMessage): Unit = {
     msg.headers foreach { case (name, values) => values.foreach(HttpHeaders.addHeader(nettyMsg, name, _)) }
     if (msg.content.length > 0) {
       HttpHeaders.setContentLength(nettyMsg, msg.content.length)
