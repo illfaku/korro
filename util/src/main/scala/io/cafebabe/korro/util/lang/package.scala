d0a16eb6f542894d82ac8233e8c0c23713da387d
Vladimir Konstantinov <illfaku@gmail.com>
2015-12-08 14:24:24 +0600

    added implicit conversions from Either to Try and Future

--- util/src/main/scala/io/cafebabe/korro/util/lang/package.scala
+++ util/src/main/scala/io/cafebabe/korro/util/lang/package.scala
@@ -1,5 +1,8 @@
 package io.cafebabe.korro.util
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
 /**
   * TODO: Add description.
   *
@@ -10,4 +13,11 @@ package object lang {
   object & {
     def unapply[A](a: A) = Some(a, a)
   }
+
+  implicit def either2try[F <: Throwable, R](either: Either[F, R]): Try[R] = either match {
+    case Right(r) => Success(r)
+    case Left(f) => Failure(f)
+  }
+
+  implicit def either2future[F <: Throwable, R](either: Either[F, R]): Future[R] = Future fromTry either2try(either)
 }
