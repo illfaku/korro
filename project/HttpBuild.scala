66aecb7f9ee127decb616f73fe4fcee70149ef55
Vladimir Konstantinov <illfaku@gmail.com>
2015-12-17 12:53:40 +0600

    release v0.2.7

--- project/HttpBuild.scala
+++ project/HttpBuild.scala
@@ -6,7 +6,7 @@ object HttpBuild extends Build {
 
   lazy val basicSettings = Seq(
     organization := "io.cafebabe.korro",
-    version := "0.2.7-SNAPSHOT",
+    version := "0.2.7",
     scalaVersion := Dependency.V.Scala
   )
 
