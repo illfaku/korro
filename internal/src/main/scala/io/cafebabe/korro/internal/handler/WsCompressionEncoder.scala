a2539d946090269e15bb4ea7ef38b47b7548253a
Vladimir Konstantinov <illfaku@gmail.com>
2016-02-17 17:35:43 +0600

    separated ws compression and decompression

--- internal/src/main/scala/io/cafebabe/korro/internal/handler/WsCompressionEncoder.scala
+++ internal/src/main/scala/io/cafebabe/korro/internal/handler/WsCompressionEncoder.scala
@@ -17,13 +17,12 @@
 package io.cafebabe.korro.internal.handler
 
 import io.cafebabe.korro.internal.ByteBufUtils.toByteBuf
-import io.cafebabe.korro.util.io.{unzipString, zipString}
-import io.cafebabe.korro.util.log.{Logging, Logger}
+import io.cafebabe.korro.util.io.zipString
+import io.cafebabe.korro.util.log.Logging
 
-import io.netty.buffer.ByteBufInputStream
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.MessageToMessageCodec
+import io.netty.handler.codec.MessageToMessageEncoder
 import io.netty.handler.codec.http.websocketx.{BinaryWebSocketFrame, TextWebSocketFrame, WebSocketFrame}
 
 import java.util
@@ -31,32 +30,22 @@ import java.util
 import scala.util.Try
 
 /**
- * Simple implementation of compression/decompression of WebSocket frames.
+ * Simple implementation of WebSocket frames compression.
  *
  * <p>Ideally it should be done using Compression Extensions for WebSocket
  * (https://tools.ietf.org/html/draft-ietf-hybi-permessage-compression-27).
- * It is implemented in Netti 5.0 but not in 4.0.
+ * It is implemented in Netty 5.0 but not in 4.0.
  *
  * @author Vladimir Konstantinov
  */
 @Sharable
-class WsCompressionHandler extends MessageToMessageCodec[WebSocketFrame, WebSocketFrame] with Logging {
+class WsCompressionEncoder extends MessageToMessageEncoder[WebSocketFrame] with Logging {
 
   override def encode(ctx: ChannelHandlerContext, msg: WebSocketFrame, out: util.List[AnyRef]): Unit = msg match {
     case f: TextWebSocketFrame =>
       Try(zipString(f.text)).map(toByteBuf).map(new BinaryWebSocketFrame(_)) recover {
         case e: Throwable =>
           log.debug("Failed to compress Text WebSocket frame. Cause: {}", e.getMessage)
-          f
-      } foreach out.add
-    case _ => out add msg
-  }
-
-  override def decode(ctx: ChannelHandlerContext, msg: WebSocketFrame, out: util.List[AnyRef]): Unit = msg match {
-    case f: BinaryWebSocketFrame =>
-      Try(unzipString(new ByteBufInputStream(f.content))).map(new TextWebSocketFrame(_)) recover {
-        case e: Throwable =>
-          log.debug("Failed to decompress Binary WebSocket frame. Cause: {}", e.getMessage)
           f
       } foreach out.add
     case _ => out add msg
--- server/src/main/scala/io/cafebabe/korro/server/handler/WsHandshakeHandler.scala
+++ server/src/main/scala/io/cafebabe/korro/server/handler/WsHandshakeHandler.scala
@@ -17,7 +17,7 @@
 package io.cafebabe.korro.server.handler
 
 import io.cafebabe.korro.internal.ChannelFutureExt
-import io.cafebabe.korro.internal.handler.{WsCompressionHandler, WsMessageCodec}
+import io.cafebabe.korro.internal.handler.{WsCompressionDecoder, WsCompressionEncoder, WsMessageCodec}
 import io.cafebabe.korro.server.config.WsConfig
 import io.cafebabe.korro.util.log.Logging
 
@@ -55,7 +55,8 @@ class WsHandshakeHandler(config: WsConfig, route: String)(implicit context: Acto
         val pipeline = channel.pipeline
         pipeline.remove("http")
         pipeline.remove(this)
-        if (config.compression) pipeline.addBefore("logging", "ws-compression", new WsCompressionHandler)
+        if (config.compression) pipeline.addBefore("logging", "ws-compression", new WsCompressionEncoder)
+        pipeline.addBefore("logging", "ws-decompression", new WsCompressionDecoder)
         pipeline.addAfter("logging", "ws-codec", new WsMessageCodec)
         pipeline.addAfter("ws-codec", "ws", new WsChannelHandler(extractHost(channel, req), route))
       } else {
