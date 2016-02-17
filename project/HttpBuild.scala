1734b13e3fcf24fdc88fc4a9df7561091e75bebc
Vladimir Konstantinov <illfaku@gmail.com>
2016-02-17 18:04:24 +0600

    next development iteration

--- project/HttpBuild.scala
+++ project/HttpBuild.scala
@@ -6,7 +6,7 @@ object HttpBuild extends Build {
 
   lazy val basicSettings = Seq(
     organization := "io.cafebabe.korro",
-    version := "0.2.11",
+    version := "0.2.12-SNAPSHOT",
     scalaVersion := Dependency.V.Scala
   )
 
