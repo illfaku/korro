ecd005cac9bc4eb5129824f2d82d086d94011631
Vladimir Konstantinov <illfaku@gmail.com>
2016-02-28 12:03:07 +0600

    set current version to 0.3.0

--- project/HttpBuild.scala
+++ project/HttpBuild.scala
@@ -6,7 +6,7 @@ object HttpBuild extends Build {
 
   lazy val basicSettings = Seq(
     organization := "org.oxydev.korro",
-    version := "0.2.13-SNAPSHOT",
+    version := "0.3.0-SNAPSHOT",
     scalaVersion := Dependency.V.Scala
   )
 
