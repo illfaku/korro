30ca7486a4fc8d97b9d252aecbf8eee270b6d6f8
Vladimir Konstantinov <illfaku@gmail.com>
2016-03-10 20:09:41 +0600

    added WsConnectionPredicate

---
+++ http/src/main/scala/org/oxydev/korro/http/tools/route/WsConnectionPredicate.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015, 2016  Vladimir Konstantinov, Yuriy Gintsyak
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.oxydev.korro.http.tools.route
+
+import org.oxydev.korro.http.api.ws.WsConnection
+import org.oxydev.korro.util.lang.{Predicate, Predicate1}
+
+/**
+ * Predicates for [[WsRouter]].
+ */
+object WsConnectionPredicate {
+
+  def apply(test: WsConnection => Boolean): Predicate1[WsConnection] = Predicate(test)
+
+  def HostIs(host: String) = apply(wc => wc.host == host || wc.host.split(':').head == host)
+  def PathIs(path: String) = apply(_.path == path)
+  def PathStartsWith(prefix: String) = apply(_.path startsWith prefix)
+}
