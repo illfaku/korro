3e0dc92503897a418344ce743d5e04e794e321be
Vladimir Konstantinov <illfaku@gmail.com>
2016-03-18 20:51:01 +0600

    added whenTerminated callback and scaladoc to ActorDependency

--- util/src/main/scala/org/oxydev/korro/util/concurrent/actor/ActorDependency.scala
+++ util/src/main/scala/org/oxydev/korro/util/concurrent/actor/ActorDependency.scala
@@ -21,9 +21,36 @@ import scala.collection.mutable
 import scala.concurrent.duration._
 
 /**
- * TODO: Add description.
+ * Mixin for <a href="http://doc.akka.io/api/akka/2.4.2/#akka.actor.Actor">`Actor`</a> that adds functionality
+ * to be dependent on another actors knowing only their paths.
  *
- * @author Vladimir Konstantinov
+ * {{{
+ *   class MyActor extends Actor with ActorDependency {
+ *     dependency("/user/my-another-actor")(_ ! Listen(self))
+ *     override def receive = ???
+ *   }
+ * }}}
+ *
+ * When dependency is added this trait schedules <a href="http://doc.akka.io/api/akka/2.4.2/#akka.actor.Identify">
+ * `Identify`</a> message to be sent to it every 1 second until
+ * <a href="http://doc.akka.io/api/akka/2.4.2/#akka.actor.ActorIdentity">`ActorIdentity`</a> is received, then it
+ * executes `whenResolved` callback with resolved `ActorRef`. Also this trait starts watching resolved actor and
+ * when/if it terminates executes `whenTerminated` callback and schedules `Identify` messages to it again.
+ *
+ * This trait is not using other threads to do its logic (except for scheduling) so you can safely use actor's context
+ * inside of `whenResolved` and `whenTerminated` callbacks.
+ *
+ * {{{
+ *   class MyActor extends Actor with ActorDependency {
+ *     dependency("/user/my-another-actor")(context become initialized(_), context become uninitialized)
+ *     def uninitialized: Receive = ???
+ *     def initialized(ref: ActorRef): Receive = ???
+ *     override def receive: Receive = uninitialized
+ *   }
+ * }}}
+ *
+ * Note: this trait overrides `unhandled` and `postStop` methods of `Actor` trait, so be sure to call their super
+ * implementations if you override them too.
  */
 trait ActorDependency extends Actor {
 
@@ -31,14 +58,21 @@ trait ActorDependency extends Actor {
 
   private val deps = mutable.Set.empty[DepInfo]
 
-  def dependency(path: String)(whenResolved: ActorRef => Unit): Unit = {
+  /**
+   * Adds dependency to actor. This method can be invoked several times to add multiple dependencies.
+   *
+   * @param path Path to actor.
+   * @param whenResolved Operation to execute when actor is resolved.
+   * @param whenTerminated Optional operation to execute when actor is terminated.
+   */
+  protected def dependency(path: String)(whenResolved: ActorRef => Unit, whenTerminated: => Unit = ()): Unit = {
     val sel = context.actorSelection(path)
     val task = scheduleIdentification(path, sel)
-    deps += new DepInfo(path, sel, whenResolved, task)
+    deps += new DepInfo(path, sel, whenResolved, () => whenTerminated, task)
   }
 
   private def scheduleIdentification(id: Any, selection: ActorSelection): Cancellable = {
-    context.system.scheduler.schedule(Duration.Zero, 2 seconds)(selection ! Identify(id))
+    context.system.scheduler.schedule(Duration.Zero, 1 seconds)(selection ! Identify(id))
   }
 
   override def unhandled(message: Any): Unit = message match {
@@ -48,7 +82,7 @@ trait ActorDependency extends Actor {
         case Some(dep) =>
           dep.task.cancel()
           context watch ref
-          dep.op(ref)
+          dep.wr(ref)
           dep.ref = Some(ref)
         case None => super.unhandled(message)
       }
@@ -56,6 +90,7 @@ trait ActorDependency extends Actor {
     case Terminated(actor) =>
       deps.find(_.ref.contains(actor)) match {
         case Some(dep) =>
+          dep.wt()
           dep.ref = None
           dep.task = scheduleIdentification(dep.id, dep.sel)
         case None => super.unhandled(message)
@@ -74,12 +109,11 @@ trait ActorDependency extends Actor {
   }
 }
 
-private [actor] class DepInfo(val id: Any, val sel: ActorSelection, val op: ActorRef => Unit, var task: Cancellable) {
-
+private [actor] class DepInfo(
+  val id: Any, val sel: ActorSelection, val wr: ActorRef => Unit, val wt: () => Unit, var task: Cancellable
+) {
   var ref: Option[ActorRef] = None
-
   override val hashCode: Int = id.hashCode
-
   override def equals(obj: scala.Any): Boolean = obj match {
     case that: DepInfo => this.id == that.id
     case _ => false
