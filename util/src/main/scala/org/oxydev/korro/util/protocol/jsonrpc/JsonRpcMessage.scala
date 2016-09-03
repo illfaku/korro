83143479945315ecc01bd44f5822298d5c3ca5a7
Vladimir Konstantinov <illfaku@gmail.com>
2016-09-03 14:23:59 +0600

    refactored JsonRpcMessage

--- util/src/main/scala/org/oxydev/korro/util/protocol/jsonrpc/JsonRpcMessage.scala
+++ util/src/main/scala/org/oxydev/korro/util/protocol/jsonrpc/JsonRpcMessage.scala
@@ -32,12 +32,12 @@ sealed trait JsonRpcMessage {
   /**
    * Adds id to this message.
    */
-  def withId(id: Int): JsonRpcMessage = withId(Some(id))
+  def withId(id: BigInt): JsonRpcMessage = withId(Option(id))
 
   /**
    * Adds optional id to this message.
    */
-  def withId(id: Option[Int]): JsonRpcMessage = this match {
+  def withId(id: Option[BigInt]): JsonRpcMessage = this match {
     case j: JsonRpcRequest => j.copy(id = id)
     case j: JsonRpcResult => j.copy(id = id)
     case j: JsonRpcError => j.copy(id = id)
@@ -57,6 +57,19 @@ object JsonRpcMessage {
   def from(json: JValue): Option[JsonRpcMessage] = {
     JsonRpcRequest.from(json) orElse JsonRpcResult.from(json) orElse JsonRpcError.from(json)
   }
+
+  private [jsonrpc] def findVersion(fields: List[(String, JValue)]): String = {
+    (for (("version", JString(version)) <- fields) yield version).headOption.getOrElse("1.0")
+  }
+
+  private [jsonrpc] def findParams(fields: List[(String, JValue)]): JValue = {
+    (for (("params", params) <- fields) yield params).headOption.getOrElse(JNothing)
+  }
+
+  private [jsonrpc] def findId(fields: List[(String, JValue)]): Option[BigInt] = {
+    (for (("id", JInt(id)) <- fields) yield id).headOption orElse
+      (for (("id", JLong(id)) <- fields) yield BigInt(id)).headOption
+  }
 }
 
 /**
@@ -69,17 +82,17 @@ object JsonRpcMessage {
  * @param params Parameters needed for this request processing. Can be any JSON value (object, array or even nothing).
  * @param id Optional request identifier.
  */
-case class JsonRpcRequest(method: String, version: String, params: JValue, id: Option[Int] = None) extends JsonRpcMessage {
+case class JsonRpcRequest(method: String, version: String, params: JValue, id: Option[BigInt] = None)
+  extends JsonRpcMessage {
 
   /**
    * This request as JSON.
    */
   override lazy val toJson = JObject(
-    List(
-      ("method", JString(method)),
-      ("version", JString(version)),
-      ("params", params)
-    ) ++ id.map("id" -> JInt(_))
+    "method"  -> JString(method),
+    "version" -> JString(version),
+    "params"  -> params,
+    "id"      -> id.map(JInt).getOrElse(JNothing)
   )
 }
 
@@ -92,20 +105,13 @@ object JsonRpcRequest {
     case JObject(fields) =>
       (for {
         ("method", JString(method)) <- fields
-      } yield JsonRpcRequest(method, findVersion(fields), findParams(fields), findId(fields))).headOption
+      } yield JsonRpcRequest(
+        method,
+        JsonRpcMessage.findVersion(fields),
+        JsonRpcMessage.findParams(fields),
+        JsonRpcMessage.findId(fields)
+      )).headOption
     case _ => None
-  }
-
-  private def findVersion(fields: List[(String, JValue)]): String = {
-    (for (("version", JString(version)) <- fields) yield version).headOption.getOrElse("1.0")
-  }
-
-  private def findParams(fields: List[(String, JValue)]): JValue = {
-    (for (("params", params) <- fields) yield params).headOption.getOrElse(JNothing)
-  }
-
-  private def findId(fields: List[(String, JValue)]): Option[Int] = {
-    (for (("id", JInt(id)) <- fields) yield id.toInt).headOption
   }
 }
 
@@ -117,60 +123,57 @@ object JsonRpcRequest {
 sealed trait JsonRpcResponse extends JsonRpcMessage
 
 /**
- * Successful JsonRpcResponse.
+ * Successful `JsonRpcResponse`.
  *
  * @param result Some JSON representing result.
  * @param id Optional response identifier.
  */
-case class JsonRpcResult(result: JValue, id: Option[Int] = None) extends JsonRpcResponse {
+case class JsonRpcResult(result: JValue, id: Option[BigInt] = None) extends JsonRpcResponse {
 
   /**
    * This response as JSON.
    */
   override lazy val toJson = JObject(
-    List(
-      ("result", result)
-    ) ++ id.map("id" -> JInt(_))
+    "result" -> result,
+    "id"     -> id.map(JInt).getOrElse(JNothing)
   )
 }
 
 object JsonRpcResult {
 
   /**
-   * Tries to convert JSON to JsonRpcResult.
+   * Tries to convert JSON to `JsonRpcResult`.
    */
   def from(json: JValue): Option[JsonRpcResult] = json match {
     case JObject(fields) =>
       (for {
         ("result", result) <- fields
-      } yield JsonRpcResult(result, findId(fields))).headOption
+      } yield JsonRpcResult(
+        result,
+        JsonRpcMessage.findId(fields)
+      )).headOption
     case _ => None
-  }
-
-  private def findId(fields: List[(String, JValue)]): Option[Int] = {
-    (for (("id", JInt(id)) <- fields) yield id.toInt).headOption
   }
 }
 
 /**
- * Unsuccessful JsonRpcResponse.
+ * Unsuccessful `JsonRpcResponse`.
  *
  * @param code Error code.
  * @param message Error message.
  * @param id Optional response identifier.
  */
-case class JsonRpcError(code: Int, message: String, id: Option[Int] = None) extends JsonRpcResponse {
+case class JsonRpcError(code: Int, message: String, id: Option[BigInt] = None) extends JsonRpcResponse {
 
   /**
    * This response as JSON.
    */
   override lazy val toJson = JObject(
-    List(
-      ("error", JObject(
-        ("code", JInt(code)),
-        ("message", JString(message))
-      ))
-    ) ++ id.map("id" -> JInt(_))
+    "error" -> JObject(
+      "code"    -> JInt(code),
+      "message" -> JString(message)
+    ),
+    "id" -> id.map(JInt).getOrElse(JNothing)
   )
 }
 
@@ -190,27 +193,37 @@ object JsonRpcError {
   /**
    * Returns JsonRpcError with pre-defined `Parse error` code.
    */
-  def ParseError(message: String, id: Option[Int] = None): JsonRpcError = apply(Codes.ParseError, message, id)
+  def ParseError(message: String = "Parse error", id: Option[BigInt] = None): JsonRpcError = {
+    JsonRpcError(Codes.ParseError, message, id)
+  }
 
   /**
-   * Returns JsonRpcError with pre-defined `Invalid Request` code.
+   * Returns JsonRpcError with pre-defined `Invalid request` code.
    */
-  def InvalidRequest(message: String, id: Option[Int] = None): JsonRpcError = apply(Codes.InvalidRequest, message, id)
+  def InvalidRequest(message: String = "Invalid request", id: Option[BigInt] = None): JsonRpcError = {
+    JsonRpcError(Codes.InvalidRequest, message, id)
+  }
 
   /**
    * Returns JsonRpcError with pre-defined `Method not found` code.
    */
-  def MethodNotFound(message: String, id: Option[Int] = None): JsonRpcError = apply(Codes.MethodNotFound, message, id)
+  def MethodNotFound(message: String = "Method not found", id: Option[BigInt] = None): JsonRpcError = {
+    JsonRpcError(Codes.MethodNotFound, message, id)
+  }
 
   /**
    * Returns JsonRpcError with pre-defined `Invalid params` code.
    */
-  def InvalidParams(message: String, id: Option[Int] = None): JsonRpcError = apply(Codes.InvalidParams, message, id)
+  def InvalidParams(message: String = "Invalid params", id: Option[BigInt] = None): JsonRpcError = {
+    JsonRpcError(Codes.InvalidParams, message, id)
+  }
 
   /**
    * Returns JsonRpcError with pre-defined `Internal error` code.
    */
-  def InternalError(message: String, id: Option[Int] = None): JsonRpcError = apply(Codes.InternalError, message, id)
+  def InternalError(message: String = "Internal error", id: Option[BigInt] = None): JsonRpcError = {
+    JsonRpcError(Codes.InternalError, message, id)
+  }
 
   /**
    * Tries to convert JSON to JsonRpcError.
@@ -219,13 +232,17 @@ object JsonRpcError {
     case JObject(fields) =>
       (for {
         ("error", JObject(error)) <- fields
-        ("code", JInt(code)) <- error
+        code <- findCode(error)
         ("message", JString(message)) <- error
-      } yield JsonRpcError(code.toInt, message, findId(fields))).headOption
+      } yield JsonRpcError(
+        code,
+        message,
+        JsonRpcMessage.findId(fields)
+      )).headOption
     case _ => None
   }
 
-  private def findId(fields: List[(String, JValue)]): Option[Int] = {
-    (for (("id", JInt(id)) <- fields) yield id.toInt).headOption
+  private def findCode(fields: List[(String, JValue)]): List[Int] = {
+    (for (("code", JInt(code)) <- fields) yield code.toInt) ++ (for (("code", JLong(code)) <- fields) yield code.toInt)
   }
 }
